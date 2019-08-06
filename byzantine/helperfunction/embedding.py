a4a0c4679d9294e26e9844587921f84248e9bd24
another1s <164536681@qq.com>
2019-08-06 11:57:40 +0800

    finished word-embedding reading code

--- byzantine/helperfunction/embedding.py
+++ byzantine/helperfunction/embedding.py
@@ -1,29 +1,32 @@
-import gensim
+from gensim.models import fasttext
 
 default_embedding = ['word2vec', 'FastText']
 
 
 class NotoriousBig:
 
-    def __int__(self, embedding_switch: 'str', embedding_addr: 'str'):
+    def __init__(self, embedding_switch: 'str', embedding_addr: 'str'):
         self.word_embedding = self.read_embedding(embedding_switch, embedding_addr)
-
         return
 
     @staticmethod
-    def read_data_tag_label():
+    def update_model(model, name):
+        model.save('../models/embedding/crawl-300d-2M-subword_' + name + '.bin')
 
-        return
+    def add_new_word(self, new_words, model):
+        model.build_vocab(new_words, update=True)
+        model.train(sentences=new_words, total_examples=len(new_words), epochs=30)
+        self.word_embedding = model
+        self.update_model(model, str(len(new_words)))
 
     @staticmethod
-    def add_new_word(word, model):
+    def read_embedding(switch, data_addr):
+        if switch == 'FastText':
+            fb_model = fasttext.load_facebook_model(path=data_addr)
+            return fb_model
 
-        return
 
-    def read_embedding(self, switch, data_addr):
-        # if switch in default_embedding:
-        #
-        # else:
-        #     print('such embedding is not available ')
-
-        return
+# config = {'switch': 'FastText', 'addr': '../models/embedding/crawl-300d-2M-subword.bin'}
+# n = NotoriousBig(embedding_switch=config['switch'], embedding_addr=config['addr'])
+# if 'equity' in n.word_embedding.wv.vocab:
+#     print(n.word_embedding.wv['equity'])

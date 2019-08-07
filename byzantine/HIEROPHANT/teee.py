93ea05102f733aa07c8a45f0974bd67e14923bdc
another1s <164536681@qq.com>
2019-08-07 16:45:02 +0800

    almost done, but model need to be uploaded

--- byzantine/HIEROPHANT/teee.py
+++ byzantine/HIEROPHANT/teee.py
@@ -1,2 +1,6 @@
 import numpy as np
+from gensim.models import fasttext
+from gensim.test.utils import datapath
 
+cap_path = datapath("crime-and-punishment.bin")
+fb_model = fasttext.load_facebook_model(cap_path)
